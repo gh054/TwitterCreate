45afd366ef315c087c9714bbcbb257bd5d8288ed
gh054 <gh054@users.noreply.github.com>
2015-09-21 00:37:23 -0400

    Update twitter-bot.py
    as you can see, a lot of code has been added
    
    more updates will follow, but right now i have to go to sleep so ill do some more tomorrow
    
    i hate unfinished/unstable code but sleep is sleep and i gotta have it

--- twitter-bot.py
+++ twitter-bot.py
@@ -1,4 +1,5 @@
 import random
+from splinter import Browser
 
 # BELOW: ALL FUNC DEFINITIONS
 
@@ -8,18 +9,24 @@ def main():
 	print "author: gh054"
 	print ""
 	print ""
-	print ""
 	print "Features:"
 	print "1. Generate Usernames"
 	print "2. Generate Passwords"
-	print "3. Create Twitter Accounts"
+	print "3. Generate Emails"
+	print "4. Create Twitter Accounts"
+	print ""
+	print ""
 	print "********************************************************"
+	print ""
+	print ""
 	proceed = raw_input("Type in a number from the Features list to continue: ")
 	if (proceed == "1"):
 		generateUsers()
 	elif (proceed == "2"):
 		generatePasses()
 	elif (proceed == "3"):
+		generateEmails()
+	elif (proceed == "4"):
 		twitterMagic()
 	else:
 		print "Program closing..."
@@ -84,17 +91,94 @@ def generatePasses():
 		x = x + 1
 	cleanup()
 
+
+def generateEmails():
+	inputnum = input("How Many Emails to Generate? ")
+	print "Generating Emails..."
+	x = 0
+	while (x < inputnum):
+		alphabet = ['a', 'b', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+		numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+		firstchar = random.choice(alphabet)
+		secondchar = random.choice(alphabet)
+		thirdchar = random.choice(alphabet)
+		fourthchar = random.choice(alphabet)
+		fifthchar = random.choice(alphabet)
+		firstnum = random.choice(numbers)
+		secondnum = random.choice(numbers)
+		thirdnum = random.choice(numbers)
+		ataoldotcom = "@aol.com"
+		emailarr = [firstchar, secondchar, thirdchar, fourthchar, fifthchar, firstnum, secondnum, thirdnum, ataoldotcom]
+		finalEmail = ''.join(str(e) for e in emailarr)
+		print finalEmail
+		femails.write(finalEmail+'\n')
+		x = x + 1
+	cleanup()
+
+
 def twitterMagic():
 	print "Twitter Magic Time!!!"
+	browser = Browser('firefox')
+	browser.visit('https://twitter.com/signup')
+	nameslist = grabNames()
+	emaillist = grabEmails()
+	passlist = grabPasses()
+	userlist = grabUsers()
+	# for each name in the list, fill the form in with data from the text files
+	# note to self - you have to set variables to loop through and pick the next name after the first is signed up
+	# STEPS!!!
+	# fill name field
+	# fill email
+	# fill password
+	# uncheck check mark
+	# click signup button
+	# (NEXT PAGE)
+	# fill username?
+	# profit
+	x = 0
+	for x in nameslist:
+		browser.fill(nameslist[x], 'full-name')
+		browser.fill(emaillist[x], 'email')
+		browser.fill(passlist[x], 'password')
+		browser.fill(userlist[x], 'username')
+		browser.uncheck('checkbox')
+		browser.find_by_name('Sign Up').first.click()
+		browser.back()
+		x = x + 1
+
+
+def grabNames():
+	returnThisList = []
+	for line in fnames:
+		returnThisList.append(line)
+
+def grabEmails():
+	returnThisList = []
+	for line in femails:
+		returnThisList.append(line)
+
+def grabPasses():
+	returnThisList = []
+	for line in fpasses:
+		returnThisList.append(line)
+
+def grabUsers():
+	returnThisList = []
+	for line in fusers:
+		returnThisList.append(line)
 
 def cleanup():
 	print "Cleaning up..."
 	fusers.close()
 	fpasses.close()
+	fnames.close()
+	femails.close()
+	print "Done cleanup() function. Now closing..."
 
 # ******************************************************************************************************
-fusers = open("twitter-user", "a+")
+fnames = open("twitter-fullnames", "a+")
+femails = open("twitter-emails", "a+")
 fpasses = open("twitter-pass", "a+")
+fusers = open("twitter-user", "a+")
 main()
-
 # ******************************************************************************************************
